a3148cc6702323060d69375355af9862a54be76f
Natanael-Mota15 <natanael.mota@csedge.org>
2022-12-10 15:15:30 -0500

    fortune teller

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import picamera
 import datetime
 import time
@@ -12,32 +12,34 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')   
 
-@app.route('/Qs', methods=['POST'])
+@app.route('/Qs', methods=['POST', 'GET'])
 def Question(): 
-    name = request.form['name']    
-    num = (random.randrange(1,18))
-    reponse = {
-        1:("It is certain."),
-        2:("It is decidedly so."),
-        3:("Without a doubt."),
-        4:("Yes - definitely."),
-        5:("You may rely on it."),
-        6:("As I see it, yes."),
-        7:("Outlook good."),
-        8:("Most likely."),
-        9:("Yes."),
-        10:("Signs point to yes."),
-        11:("Reply hazy, try again another time."),
-        12:("Ask again later."),
-        13:("Better not tell you now."),
-        14:("Cannot predict now."),
-        15:("Concentrate and ask again another time."),
-        16:("Don't count on it."),
-        17:("My reply is no."),
-        18:("My sources say no.")
-        }
-    display = f'{name},{reponse[num]}'
-    sense.show_message((display),text_colour=(0,0,255),scroll_speed=0.05)
+    if request.method == 'POST':
+            
+        name = request.form['name']    
+        num = (random.randrange(1,18))
+        reponse = {
+            1:("It is certain."),
+            2:("It is decidedly so."),
+            3:("Without a doubt."),
+            4:("Yes - definitely."),
+            5:("You may rely on it."),
+            6:("As I see it, yes."),
+            7:("Outlook good."),
+            8:("Most likely."),
+            9:("Yes."),
+            10:("Signs point to yes."),
+            11:("Reply hazy, try again another time."),
+            12:("Ask again later."),
+            13:("Better not tell you now."),
+            14:("Cannot predict now."),
+            15:("Concentrate and ask again another time."),
+            16:("Don't count on it."),
+            17:("My reply is no."),
+            18:("My sources say no.")
+            }
+        display = f'{name},{reponse[num]}'
+        sense.show_message((display),text_colour=(0,0,255),scroll_speed=0.05)
     return render_template('Qs.html')
 
 @app.route('/future')
